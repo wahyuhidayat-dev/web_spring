9aec9dcfc5d00e6392e4bb87fa803b78c8437a1d
wahyu hidayat <wahyu.hidayat@lottemart.co.id>
2022-08-29 09:33:21 +0700

    udemy spring boot

---
+++ src/main/java/com/example/demo/models/Persons.java
@@ -0,0 +1,11 @@
+package com.example.demo.models;
+
+import lombok.Data;
+
+@Data
+public class Persons {
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String phone;
+}
